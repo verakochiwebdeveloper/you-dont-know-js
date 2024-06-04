617fa4911d9c28cfb7bbf33020c0c466d40147c5
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-06-04 08:14:30 +0300

    fix bg

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
+// Восстановление порядка книг
 const booksContainer = document.querySelectorAll('.book');
 const mainContent = document.querySelector('main');
 booksContainer.forEach(book => {
-    mainContent.appendChild(book);
+    mainContent.append(book);
 });
 
-document.body.style.backgroundImage = 'url("image/new_background.jpg")';
+// Замена картинки заднего фона
+document.body.style.backgroundImage = 'url("./image/you-dont-know-js.jpg")';
 
+// Исправление заголовка в книге 3
 const book3Title = document.querySelector('div.book h2 a[href*="this%20и%20Прототипы%20Объектов"]');
 book3Title.textContent = "Книга 3. this и Прототипы Объектов";
 
+// Удаление рекламы
 const advSection = document.querySelector('.adv');
 advSection.remove();
 
+// Восстановление порядка глав во второй и пятой книге
 const book2Chapters = document.querySelectorAll('div.book:nth-child(2) li');
 const book5Chapters = document.querySelectorAll('div.book:nth-child(5) li');
 
 book2Chapters.forEach(chapter => {
-    document.querySelector('div.book:nth-child(2) ul').appendChild(chapter);
+    document.querySelector('div.book:nth-child(2) ul').append(chapter);
 });
 
 book5Chapters.forEach(chapter => {
-    document.querySelector('div.book:nth-child(5) ul').appendChild(chapter);
+    document.querySelector('div.book:nth-child(5) ul').append(chapter);
 });
 
+// Добавление новой главы в шестую книгу
 const newChapter = document.createElement('li');
 newChapter.textContent = "Глава 8: За пределами ES6";
-document.querySelector('div.book:nth-child(6) ul').appendChild(newChapter);
+document.querySelector('div.book:nth-child(6) ul').append(newChapter);
